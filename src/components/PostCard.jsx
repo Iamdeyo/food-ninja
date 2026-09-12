9a427956870f093636c78733efcb656bd1993208
iamdeyo <dtechlord@gmail.com>
2026-09-12 13:19:48 +0100

    blog

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,31 +1,30 @@
+import { Link } from "react-router";
 import post1 from "../assets/post1.png";
 
-export default function PostCard() {
+export default function PostCard({ post }) {
   return (
-    <div className="">
+    <Link to={`/post/${post.id}`} className="">
       <img
         src={post1}
         alt="post one image"
         className="max-h-69.5 w-full object-cover"
       />
       <div className="pt-5">
-        <div>
-          <span className="bg-primary text-white inline-block px-4 py-1 rounded-lg text-sm">
-            Travel
-          </span>
+        <div className="flex gap-1">
+          {post.tags.map((tag, index) => (
+            <span
+              key={index}
+              className="bg-primary text-white inline-block px-4 py-1 rounded-lg text-sm"
+            >
+              {tag}
+            </span>
+          ))}
         </div>
         <h2 className="mt-2 text-xl md:text-2xl font-merriweather font-bold">
-          What Traveling Greece For 2 Weeks Taught Me About Life
+          {post.title}
         </h2>
         <p className="text-sm text-gray-600 mt-1">Jun 21, 2021 • 11 min read</p>
-        <p className="text-gray-600 mt-2 line-clamp-4">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Diam mollis
-          Diam mollis lectus vitae nulla malesuada amet purus sed. A condimentum
-          tempus a egestas sodales diam cras. egestas sodales diam cras. Lorem
-          ipsum dolor sit amet, consectetur adipiscing elit. Diam mollis lectus
-          vitae nulla malesuada amet purus sed. A condimentum tempus a egestas
-          sodales diam cras.
-        </p>
+        <p className="text-gray-600 mt-2 line-clamp-4">{post.body}</p>
         <div className="mt-2 flex items-center gap-3">
           <img
             src={post1}
@@ -37,6 +36,6 @@ export default function PostCard() {
           </span>
         </div>
       </div>
-    </div>
+    </Link>
   );
 }
--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,11 +1,29 @@
+import { useEffect, useState } from "react";
 import PostCard from "./PostCard";
 
 export default function Posts() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const responce = await fetch("https://dummyjson.com/posts", {
+        method: "GET",
+      });
+
+      const resData = await responce.json();
+      setData(resData);
+      console.log(resData);
+    };
+
+    fetchPosts();
+  }, []);
+
   return (
     <>
-      <section className="mt-16 flex flex-col md:flex-row gap-10">
-        <PostCard />
-        <PostCard />
+      <section className="mt-16 flex flex-col md:grid grid-cols-2 gap-10">
+        {data &&
+          data.posts.map((post) => <PostCard post={post} key={post.id} />)}
+        {/* <PostCard /> */}
       </section>
       <ul className="mt-10 flex items-end justify-center gap-6">
         <li className="font-bold size-10.5 text-white bg-primary flex justify-center items-center rounded-full">
