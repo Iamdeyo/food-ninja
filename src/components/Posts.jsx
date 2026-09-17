94948fe6fde437fd495787f61db8141e200c62ea
iamdeyo <dtechlord@gmail.com>
2026-09-17 18:55:36 +0100

    Implement pagination and loading states in Posts component; enhance PostPage with error handling and data fetching

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -3,20 +3,93 @@ import PostCard from "./PostCard";
 
 export default function Posts() {
   const [data, setData] = useState(null);
+  const [isloading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+  const [page, setPage] = useState(1);
+  const [skip, setSkip] = useState(0);
+  const limit = 10;
+
+  const nextPage = () => {
+    setPage((prev) => prev + 1);
+    setSkip((prev) => prev + limit);
+  };
+
+  const prevPage = () => {
+    setPage((prev) => (prev > 1 ? prev - 1 : prev));
+    setSkip((prev) => (prev > 0 ? prev - limit : prev));
+  };
 
   useEffect(() => {
     const fetchPosts = async () => {
-      const responce = await fetch("https://dummyjson.com/posts", {
-        method: "GET",
-      });
+      try {
+        // setIsLoading(true);
+        setIsError(false);
 
-      const resData = await responce.json();
-      setData(resData);
-      console.log(resData);
+        const response = await fetch(
+          `https://dummyjson.com/posts?limit=${limit}&skip=${skip}`,
+        );
+
+        if (!response.ok) {
+          throw new Error("Error!");
+        }
+
+        const resData = await response.json();
+
+        if (data !== null) {
+          setData((prev) => ({
+            ...prev,
+            posts: [...resData.posts, ...prev.posts],
+          }));
+        } else {
+          setData(resData);
+        }
+      } catch (error) {
+        setIsError(true);
+      } finally {
+        setIsLoading(false);
+      }
     };
 
     fetchPosts();
+  }, [limit, skip]);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition = window.innerHeight + window.scrollY;
+      const pageHeight = document.documentElement.scrollHeight;
+
+      // if (scrollPosition >= pageHeight) {
+      //   console.log("Reached the bottom!");
+
+      //   nextPage();
+      // }
+      if (window.scrollY === 0) {
+        nextPage();
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
   }, []);
+
+  if (isloading) {
+    return (
+      <div className="py-20 font-merriweather text-5xl text-center">
+        <div className="animate-pulse">Loading......</div>
+      </div>
+    );
+  }
+
+  if (data === null || isError) {
+    return (
+      <>
+        <div> Posts Not Found! </div>
+      </>
+    );
+  }
 
   return (
     <>
@@ -25,19 +98,22 @@ export default function Posts() {
           data.posts.map((post) => <PostCard post={post} key={post.id} />)}
         {/* <PostCard /> */}
       </section>
+      skip: {skip}
       <ul className="mt-10 flex items-end justify-center gap-6">
+        <li
+          onClick={prevPage}
+          className="font-bold size-10.5 text-gray-600 bg-gray-300 flex justify-center items-center rounded-full cursor-pointer"
+        >
+          {"<"}
+        </li>
         <li className="font-bold size-10.5 text-white bg-primary flex justify-center items-center rounded-full">
-          1
+          {page}
         </li>
-        <li className="font-bold size-10.5 text-gray-600 bg-gray-300 flex justify-center items-center rounded-full">
-          2
-        </li>
-        <li className="font-bold size-10.5 text-gray-600 bg-gray-300 flex justify-center items-center rounded-full">
-          3
-        </li>
-        ....
-        <li className="font-bold size-10.5 text-gray-600 bg-gray-300 flex justify-center items-center rounded-full">
-          13
+        <li
+          onClick={nextPage}
+          className="font-bold size-10.5 text-gray-600 bg-gray-300 flex justify-center items-center rounded-full cursor-pointer"
+        >
+          {">"}
         </li>
       </ul>
     </>
--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -1,11 +1,89 @@
 import { useParams } from "react-router";
+import post1 from "../assets/post1.png";
+import { useEffect, useState } from "react";
 
 export default function PostPage() {
+  const [data, setData] = useState(null);
+  const [isloading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
   const params = useParams();
 
+  useEffect(() => {
+    const fetchPost = () => {
+      fetch("https://dummyjson.com/posts/" + params.slug)
+        .then((res) => {
+          if (!res.ok) {
+            throw new Error(`HTTP error: ${res.status}`);
+          }
+
+          return res.json();
+        })
+        .then((data) => {
+          setData(data);
+        })
+        .catch((err) => {
+          setIsError(true);
+        })
+        .finally(() => {
+          setIsLoading(false);
+        });
+    };
+
+    fetchPost();
+  }, []);
+
+  if (isloading) {
+    return (
+      <div className="py-20 font-merriweather text-5xl text-center">
+        <div className="animate-pulse">Loading......</div>
+      </div>
+    );
+  }
+
+  if (data === null || isError) {
+    return (
+      <>
+        <div> Post Not Found! </div>
+      </>
+    );
+  }
+
   return (
-    <div className="font-merriweather text-7xl text-center pt-10">
-      PostPage with slug: {params.slug}
+    <div className="max-w-5xl mx-auto pt-10">
+      <div className="flex gap-3">
+        {data.tags.map((tag, index) => (
+          <span
+            key={index}
+            className="bg-primary text-white inline-block px-4 py-1 rounded-lg text-sm"
+          >
+            {tag}
+          </span>
+        ))}
+      </div>
+
+      <h2 className="mt-4 text-4xl md:text-5xl font-merriweather font-bold">
+        {data.title}
+      </h2>
+      <p className="text-sm text-gray-600 mt-3">Jun 21, 2021 • 11 min read</p>
+
+      <img
+        src={post1}
+        alt="post one image"
+        className="max-h-200 w-full object-cover mt-5"
+      />
+
+      <div className="mt-5 flex items-center gap-3">
+        <img
+          src={post1}
+          alt="George Costanza photo"
+          className="size-10 rounded-full"
+        />
+        <span className="text-sm font-bold font-merriweather">
+          George Costanza
+        </span>
+      </div>
+
+      <p className="text-gray-600 mt-10 line-clamp-4">{data.body}</p>
     </div>
   );
 }
